c5d2372c27764307c02161a6fa6e44e3f8d9e886
tillaczel <tillaczel@gmail.com>
2025-11-23 21:10:15 +0100

    google search pictures

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import { colors } from '@/config/colors'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const siteUrl = 'https://tillaczel.net'
+
 export const metadata: Metadata = {
   title: 'Till Aczel',
   description: 'Personal academic website',
@@ -21,6 +23,28 @@ export const metadata: Metadata = {
       { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   },
+  openGraph: {
+    title: 'Till Aczel',
+    description: 'PhD student at ETH Zurich',
+    url: siteUrl,
+    siteName: 'Till Aczel',
+    images: [
+      {
+        url: `${siteUrl}/profile.jpg`,
+        width: 1200,
+        height: 630,
+        alt: 'Till Aczel',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Till Aczel',
+    description: 'PhD student at ETH Zurich',
+    images: [`${siteUrl}/profile.jpg`],
+  },
 }
 
 export default function RootLayout({
@@ -28,8 +52,36 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+  const profileImageUrl = `${siteUrl}${basePath}/profile.jpg`
+
+  const personSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Till Aczel',
+    description: 'PhD student at ETH Zurich',
+    image: profileImageUrl,
+    url: `${siteUrl}${basePath}`,
+    email: 'taczel@ethz.ch',
+    jobTitle: 'PhD Student',
+    worksFor: {
+      '@type': 'Organization',
+      name: 'ETH Zurich',
+    },
+    sameAs: [
+      'https://github.com/tillaczel',
+      'https://www.linkedin.com/in/till-aczel/',
+    ],
+  }
+
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(personSchema) }}
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider>
           <Navigation />
